2c3659aa04468c2aeedfd958a538e4ee5bfdbaa7
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-12-15 22:42:10 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<string>
+
+using namespace std ;
+
+int main()
+{
+    string a ,  b ;
+    cin >> a >> b ;
+    cout << a.size() << " " << b.size() << endl ;
+    cout << a << " " << b << endl ;
+
+    return 0;
+}
